c1130ab218130a0e67dcc308b6c78a2a02b429fc
nasirhm <nasirhussainm14@gmail.com>
2020-06-18 04:39:59 +0500

    Fixed permission: made directories executable along with remove x from .c files.

--- chapter4/linux-repl-prompt.c
+++ chapter4/linux-repl-prompt.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <editline/readline.h>
+#include <editline/history.h>
 
-int main(int argc, char** argv){
-    puts("Lispy version 0.0.0.0.1");
-    puts("Press CTRL-C to exit.\n");
+int main(int argc, char** argv) {
 
-    while(1){
-	char* input = readline("lispy> ");
-   	printf("You're %s\n",input);
-	free(input);
-   }
+      /* Print Version and Exit Information */
+      puts("Lispy Version 0.0.0.0.1");
+      puts("Press Ctrl+c to Exit\n");
 
+      /* In a never ending loop */
+      while (1) {
+
+      /* Output our prompt and get input */
+      char* input = readline("lispy> ");
+
+      /* Add input to history */
+      add_history(input);
+
+      /* Echo input back to user */
+      printf("No you're a %s\n", input);
+
+      /* Free retrived input */
+      free(input);
+
+      }
+
+     return 0;
 }
+
+
